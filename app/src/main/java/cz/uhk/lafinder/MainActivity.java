263ba86b400fbd992a7622520afaf83f9a2c7482
HAK JIRI <petrjanpetr@seznam.cz>
2021-01-15 11:35:11 +0100

    Zapojeni RSSI do systému - zatím na listview

--- app/src/main/java/cz/uhk/lafinder/MainActivity.java
+++ app/src/main/java/cz/uhk/lafinder/MainActivity.java
@@ -1,12 +1,28 @@
 package cz.uhk.lafinder;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import java.io.BufferedReader;
@@ -21,6 +37,14 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText et;
 
+
+    public static final int REQUEST_ACCES_COARSE_LOCATION = 1;
+    public static final int REQUEST_ENABLE_BLUETOOTH =11;
+    private ListView devicesList;
+    private Button blueButton;
+    private BluetoothAdapter blue;
+    private ArrayAdapter<String> listAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +53,67 @@ public class MainActivity extends AppCompatActivity {
 
         Thread myThread = new Thread(new MyServerThread());
         myThread.start();
+
+        blue = BluetoothAdapter.getDefaultAdapter();
+        devicesList = (ListView) findViewById(R.id.devicesList);
+        blueButton = (Button) findViewById(R.id.blueButton);
+
+        listAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1);
+        devicesList.setAdapter(listAdapter);
+
+        checkBluetoothState();
+
+        blueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(blue != null && blue.isEnabled()){
+                    if(checkCoarseLocaetionPremision()){
+                        listAdapter.clear();
+                        blue.startDiscovery();
+                    }
+                }else{
+                    checkBluetoothState();
+                }
+
+            }
+        });
+        checkCoarseLocaetionPremision();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(devicesFoundReciver, new IntentFilter(BluetoothDevice.ACTION_FOUND));
+        registerReceiver(devicesFoundReciver, new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_STARTED));
+        registerReceiver(devicesFoundReciver, new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED));
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        unregisterReceiver(devicesFoundReciver);
+    }
+
+    private boolean checkCoarseLocaetionPremision(){
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+           != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                    REQUEST_ACCES_COARSE_LOCATION);
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    private void checkBluetoothState(){
+        if(blue.isEnabled()){
+            if(!blue.isDiscovering()){
+                blueButton.setEnabled(true);
+            }
+        }else{
+            Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableIntent,REQUEST_ENABLE_BLUETOOTH);
+        }
     }
 
     class MyServerThread implements Runnable{
@@ -43,6 +128,8 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void run() {
             try  {
+
+                blue = BluetoothAdapter.getDefaultAdapter();
                 ss = new ServerSocket(7801);
                 while (true){
                     s = ss.accept();
@@ -56,6 +143,14 @@ public class MainActivity extends AppCompatActivity {
                             Toast.makeText(getApplicationContext(),message,Toast.LENGTH_SHORT).show();
                         }
                     });
+
+
+
+
+                    //modlimodli
+
+                    BackgroundTask bt = new BackgroundTask();
+                    bt.execute(message);
 
                 }
 
@@ -95,4 +190,35 @@ public class MainActivity extends AppCompatActivity {
             return null;
         }
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+        if(requestCode == REQUEST_ENABLE_BLUETOOTH){
+            checkBluetoothState();
+        }
+    }
+    /*
+    @Override
+    protected void onRequestPermissionsResult(int requestCode, @NonNull String[] premissions, @NonNull int[] grandResluts){
+        super.onRequestPermissionsResult(requestCode,premissions,grandResluts);
+        switch (requestCode){
+            case REQUEST_ACCES_COARSE_LOCATION :
+                if(grandResluts.length > 0 && grandResluts)
+        }
+    }*/
+
+    private final BroadcastReceiver devicesFoundReciver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if(BluetoothDevice.ACTION_FOUND.equals(action)){
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                int  rssi = intent.getShortExtra(BluetoothDevice.EXTRA_RSSI,Short.MIN_VALUE);
+                listAdapter.add(device.getName() + "\n" + device.getAddress() + "\n" + rssi);
+            }
+        }
+    };
+
+
 }
